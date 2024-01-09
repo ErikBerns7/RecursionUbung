b41620028af8be76410a09d7955defb8913ffcdc
Erik Berns <erik.berns@gmx.de>
2024-01-09 03:08:57 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Recursion(10);
+//Methode die eine Zahl runterzählt ie von 10 auf 1 oder 99 auf 1
+int Recursion(int number)
+{
+    Console.WriteLine(number);
+    if (number == 1) return 1;
+    return number = Recursion(number - 1);
+}
